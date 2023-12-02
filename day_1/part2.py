33ec6c06afed920448f129a0abbe4928af8e7fab
Dhanji97 <m-a.dhanji@hotmail.com>
2023-12-02 17:49:10 +0000

    refactored day1 part 2 to deduplicate code and simplify

--- day_1/part2.py
+++ day_1/part2.py
@@ -10,82 +10,74 @@ number_dict = {
     'nine': '9'
 }
 
-def find_front_digit_index(line: str):
-    front_pointer = 0
-    found_first = False
-    while not found_first:
-        if line[front_pointer].isdigit():
-            return front_pointer, line[front_pointer]
-        else:
-            front_pointer += 1
-            if front_pointer == len(line):
-                return float('inf'), None
 
-def find_end_digit_index(line: str):
-    end_pointer = -1
-    found_last = False
-    while not found_last:
-        if line[end_pointer].isdigit():
-            return (len(line) + end_pointer), line[end_pointer]
-        else:
-            end_pointer -= 1
-            if abs(end_pointer) > len(line):
-                return -1, None
-
-def find_first_number_word(line: str):
-    earliest_index = float('inf')
-    earliest_number = None
-
-    for key in number_dict.keys():
-        index = line.find(key)
-        if index != -1 and index < earliest_index:
-            earliest_index = index
-            earliest_number = number_dict[key]
-
-    if earliest_number is not None:
-        return earliest_index, earliest_number
-    else:
+def find_digit(line: str, from_front: bool = True):
+    """A function that finds the first digit in a given string. It will return the index of the digit and the digit found, as a string.
+    The function will search from the front of the string when from_front is true or not provided, if no digit is found an infinite float and None is returned.
+    The function will search from the back of the string when fro_front is False, if no digit is found -1 and None are returned."""
+    string_len = len(line)
+    index_array = range(string_len) if from_front else reversed(range(string_len))
+    for i in index_array:
+        if line[i].isdigit():
+            return i, line[i]
+    if from_front:
+        print ('flag 1')
         return float('inf'), None
-
-def find_last_number_word(line:str): 
-    latest_index = -1
-    latest_number = None
-
-    for key in number_dict.keys():
-        index = line.rfind(key)
-        if index != -1 and index > latest_index:
-            latest_index = index
-            latest_number = number_dict[key]
-
-    if latest_number is not None:
-        return latest_index, latest_number
     else:
         return -1, None
 
-def answer(txt_file):
+
+def find_number_word(line: str, from_front: bool = True):
+    """A function that finds the first number in english for a given string. It will return the index of the number and the digit of the number found as a string.
+    The function will search from the front of the string when from_front is true or not provided, if no number is found an infinite float and None is returned.
+    The function will search from the back of the string when from_front is False, if no number is found -1 and None are returned."""
+    index_func = line.find if from_front else line.rfind
+    index_value = float('inf') if from_front else -1
+    result_index = index_value
+    result_number = None
+
+    for key in number_dict.keys():
+        index = index_func(key)
+        if index != -1:
+            if (from_front and index < result_index) or (not from_front and index > result_index):
+                result_index = index
+                result_number = number_dict[key]
+
+    if result_number is not None:
+        return result_index, result_number
+    else:
+        return index_value, None
+
+
+def evaluate_first_index(index_1: float or int, number_1: str, index_2: float or int, number_2: str, is_smallest: bool = True):
+    """A function that compares the index of the from the numbers found in the string and returns the number with the required condition.
+    If is_smallest is True or not provided it will compare for the smallest index, i.e. the index that is closest to the start.
+    if is_smallest is False it will compare to find the biggest index, i.e. the index that is closest to the end. """
+    if (is_smallest and index_1 < index_2) or (not is_smallest and index_1 > index_2 ):
+        return number_1
+    else:
+        return number_2
+
+def day_1_part_2_answer(txt_file):
     file = open(txt_file, 'r')
     Lines = [line.strip() for line in file.readlines()]
     total_value = 0
 
     for line in Lines:
+        front_digit_index, front_digit_element = find_digit(line)
+        front_word_index, front_word_element = find_number_word(line)
 
-        front_digit_index, front_digit_element = find_front_digit_index(line)
-        front_word_index, front_word_element = find_first_number_word(line)
-        if front_digit_index < front_word_index:
-            first_number = front_digit_element
-        else:
-            first_number = front_word_element
+        last_digit_index, last_digit_element = find_digit(line, False)
+        last_word_index, last_word_element = find_number_word(line, False)
 
-        last_digit_index, last_digit_element = find_end_digit_index(line)
-        last_word_index, last_word_element = find_last_number_word(line)
-        if last_digit_index > last_word_index:
-            last_number = last_digit_element
-        else:
-            last_number = last_word_element
+        first_number = evaluate_first_index(front_digit_index, front_digit_element, front_word_index, front_word_element)
+        last_number = evaluate_first_index(last_digit_index, last_digit_element, last_word_index, last_word_element, False)
+
         line_value = int(first_number + last_number)
         total_value += line_value
 
     return total_value
 
-aws = answer('day1_text.txt')
-print(aws)
+
+answer = day_1_part_2_answer('day1_text.txt')
+print(answer)
