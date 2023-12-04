a456a712437f752b03556abefe09b8a1779f078e
Dhanji97 <m-a.dhanji@hotmail.com>
2023-12-04 16:29:38 +0000

    removed need for dictionary in day_2 solution

--- day_2/part1.py
+++ day_2/part1.py
@@ -6,16 +6,15 @@ def day_2_part_1_answer(file_name):
     lines = file.readlines()
     total = 0
     for line in lines:
-        line_dict = {}
-        line_dict['game_number'] = int(re.findall(r"(\d+):", line)[0])
+        game_number = int(re.findall(r"(\d+):", line)[0])
         red_values = [eval(i) for i in re.findall(r"(\d+) red", line)]
         green_values = [eval(i) for i in re.findall(r"(\d+) green", line)]
         blue_values = [eval(i) for i in re.findall(r"(\d+) blue", line)]
-        line_dict['max_red'] = max(red_values)
-        line_dict['max_green'] = max(green_values)
-        line_dict['max_blue'] = max(blue_values)
-        if (line_dict['max_red'] <= 12) and (line_dict['max_green'] <= 13) and (line_dict['max_blue'] <= 14):
-            total += line_dict['game_number']
+        max_red = max(red_values)
+        max_green = max(green_values)
+        max_blue = max(blue_values)
+        if (max_red <= 12) and (max_green <= 13) and (max_blue <= 14):
+            total += game_number
     return total
 
 
--- day_2/part2.py
+++ day_2/part2.py
@@ -6,15 +6,13 @@ def day_2_part_1_answer(file_name):
     lines = file.readlines()
     total = 0
     for line in lines:
-        game_dict = {}
-        game_dict['game_number'] = int(re.findall(r"(\d+):", line)[0])
         red_values = [eval(i) for i in re.findall(r"(\d+) red", line)]
         green_values = [eval(i) for i in re.findall(r"(\d+) green", line)]
         blue_values = [eval(i) for i in re.findall(r"(\d+) blue", line)]
-        game_dict['max_red'] = max(red_values)
-        game_dict['max_green'] = max(green_values)
-        game_dict['max_blue'] = max(blue_values)
-        power_min_set = game_dict['max_blue']*game_dict['max_green']*game_dict['max_red']
+        max_red = max(red_values)
+        max_green = max(green_values)
+        max_blue = max(blue_values)
+        power_min_set = max_blue*max_green*max_red
         total += power_min_set
 
     return total
